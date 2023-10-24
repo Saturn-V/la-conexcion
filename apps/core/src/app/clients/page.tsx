e4b848a0b2b1ee8e013f3e0e190a2b7e33c65926
Saturn-V <alexaaronpena@gmail.com>
2023-10-23 20:56:04 -0700

    [LA-18] Update clients page

--- apps/core/src/app/clients/page.tsx
+++ apps/core/src/app/clients/page.tsx
@@ -10,8 +10,8 @@ import styles from './page.module.css'
 
 export default function About() {
     return <div className={styles.page}>
-        <ClientCard name="Hustle Art" plan="Startup" assetPath='hustle-art' />
-        <ClientCard name="Best Auto Repair" plan="Premium" assetPath='best-auto-repair' />
+        <ClientCard name="Hustle Art" plan="Startup" assetPath='hustle-art' href='http://hustleart.sjpage.com' />
+        <ClientCard name="Best Auto Repair" plan="Premium" assetPath='best-auto-repair' href='http://bestautorepair.sjpage.com' />
     </div>
 }
 
@@ -19,13 +19,14 @@ interface ClientCardProps {
     name: string
     plan: string
     assetPath: string
+    href: string
 }
 
-function ClientCard({name, plan, assetPath}: ClientCardProps) {
+function ClientCard({name, plan, assetPath, href}: ClientCardProps) {
     return <Card>
     <Tabs defaultValue="desktop">
         <div className={styles.header}>
-            <p><span className={classNames(styles.title, styles.medium)}>{name}</span> / Visit</p>
+            <p><span className={classNames(styles.title, styles.medium)}>{name}</span> / <a href={href} target="_blank" rel="noopener noreferrer">Visit</a></p>
             <Badge variant="outline" className={styles.badge}>{plan}</Badge>
             <div className={styles.tabs}>
 
@@ -43,7 +44,7 @@ function ClientCard({name, plan, assetPath}: ClientCardProps) {
                     src={`/images/clients/${assetPath}/desktop.png`}
                     alt={`Screenshot of the ${name} Website on Desktop`}
                     width={1080}
-                    height={607}
+                    height={600}
                     priority
                 />
             </TabsContent>
